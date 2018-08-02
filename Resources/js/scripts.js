5c2c67c43f70e25c3848aeee0ef46c7386e131e3
Sebastian Neferu <neferusebastian@gmail.com>
2018-08-02 21:38:45 +0200

    Fixed smooth navigation issue

--- Resources/js/scripts.js
+++ Resources/js/scripts.js
@@ -24,6 +24,20 @@ $(document).ready(function() {
 
     /* Navigation scroll */
 
+    $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({scrollTop: target.offset().top}, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+
     /* Animation on scroll */
 
     $('.js--wp-1').waypoint(function(direction) {
